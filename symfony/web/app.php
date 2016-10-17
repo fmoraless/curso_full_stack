386ebfe57f3b192979006b81df8b757dc7501817
Claudia <Claudia@Claudia-PC>
2016-10-16 22:36:44 -0300

    Configurar cabeceras.

--- symfony/web/app.php
+++ symfony/web/app.php
@@ -1,7 +1,14 @@
 <?php
 
 use Symfony\Component\HttpFoundation\Request;
-
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+$method = $_SERVER["REQUEST_METHOD"];
+if($method == "OPTIONS"){
+    die();
+}
 /**
  * @var Composer\Autoload\ClassLoader
  */
